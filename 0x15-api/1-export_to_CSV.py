9e7e4fd3d56437598e9c2e90cc883078e6889b05
jeffreykanemitsu <jeffreykanemitsu@gmail.com>
2018-01-03 07:09:46 +0000

    changed name to username

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -11,11 +11,11 @@ import csv
 if __name__ == "__main__":
     EMPLOYEE_ID = argv[1]
     URL = 'https://jsonplaceholder.typicode.com'
-    USER = requests.get(URL + '/users/' + EMPLOYEE_ID).json().get('name')
+    USERNAME = requests.get(URL + '/users/' + EMPLOYEE_ID).json().get('username')
     TODO_LIST = requests.get(URL + '/todos?userId=' + EMPLOYEE_ID).json()
 
     with open('{}.csv'.format(EMPLOYEE_ID), 'w') as f:
         writer = csv.writer(f, quoting=csv.QUOTE_ALL)
 
         for task in TODO_LIST:
-            writer.writerow([EMPLOYEE_ID, USER, task.get('completed'), task.get('title')])
+            writer.writerow([EMPLOYEE_ID, USERNAME, task.get('completed'), task.get('title')])
